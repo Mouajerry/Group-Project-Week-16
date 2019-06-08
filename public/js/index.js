0d5e04b54dd840c0f533283b875fc39d6f42d438
Jerry <Jerrymoua25@gmail.com>
2019-06-07 19:57:48 -0500

    api

--- public/js/index.js
+++ public/js/index.js
@@ -1,99 +1,134 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
+var $exampleText = $("#name");
+var $exampleDescription = $("#email");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
+var ingredientStorage = [];
+var userIngredients = [];
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
+    getExamples: function() {
+      return $.ajax({
+        url: "api/get-recipes/",
+        type: "POST",
+        data: {ingredients:[]}
+      }).then(function(response){
+        userIngredients = response[0]
+        // console.log("user ingredients: ",userIngredients)
+  
+        for (let i = 1; i < response.length; i++) {
+  
+          let ingredientArray = response[i].ingredients,
+              index = i - 1,
+              label = response[i].label,
+              image = response[i].image,
+              calories = response[i].calories,
+              servings = response[i].servings,
+              time = response[i].time,
+              url = response[i].url,
+              wrapper = $("<div>"),
+              row = $("<div>"),
+              bigColumn = $("<div>"),
+              smallColumn = $("<div>"),
+              img = $("<img>"),
+              p = $("<p>"),
+              saveButton = $("<button type=\"button\"><i class=\"fas fa-heart saveHeart\"></i></button>")
+  
+  
+  
+          ingredientStorage.push(ingredientArray)
+          // console.log(ingredientStorage)
+  
+          //Build the list of recipe results dynamically
+             wrapper.addClass("apiRecipe")
+                 row.addClass("labelRow")
+           bigColumn.addClass("labelColumn")
+         smallColumn.addClass("buttonColumn")
+                   p.addClass("recipeLabel")
+                    .attr("data-id", index)
+                    .text(label)
+                img.addClass("recipeImg openRecipe")
+                    .attr("data-id", index)
+                    .attr("src", image)
+                    .attr("data-src", url)
+                    .attr("data-calories", calories)
+                    .attr("data-servings", servings)
+                    .attr("data-time", time)
+          saveButton.addClass("saveBtn saveRecipe notsaved")
+                    .attr("data-saved", "false")
+                    .attr("data-heart", index)
+  
+  
+          bigColumn.append(p)
+          smallColumn.append(saveButton)
+          row.append(smallColumn, bigColumn)
+          wrapper.append(img, row)
+          $("#recipeResults").append(wrapper)  
+        }
+  
+  
+      })
+    },
+    deleteExample: function(id) {
+      return $.ajax({
+        url: "api/examples/" + id,
+        type: "DELETE"
+      });
+    }
+  };
+  // refreshExamples gets new examples from the db and repopulates the list
+  var refreshExamples = function() {
+    API.getExamples().then(function(data) {
+      var $examples = data.map(function(example) {
+        var $a = $("<a>")
+          .text(example.text)
+          .attr("href", "/example/" + example.id);
+        var $li = $("<li>")
+          .attr({
+            class: "list-group-item",
+            "data-id": example.id
+          })
+          .append($a);
+        var $button = $("<button>")
+          .addClass("btn btn-danger float-right delete")
+          .text("ｘ");
+        $li.append($button);
+        return $li;
+      });
+      $exampleList.empty();
+      $exampleList.append($examples);
     });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
+  };
+  // handleFormSubmit is called whenever we submit a new example
+  // Save the new example to the db and refresh the list
+  var handleFormSubmit = function(event) {
+    event.preventDefault();
+    
   var example = {
     text: $exampleText.val().trim(),
     description: $exampleDescription.val().trim()
   };
-
   if (!(example.text && example.description)) {
     alert("You must enter an example text and description!");
     return;
   }
-
   API.saveExample(example).then(function() {
     refreshExamples();
   });
-
   $exampleText.val("");
   $exampleDescription.val("");
 };
-
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
 var handleDeleteBtnClick = function() {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
-
   API.deleteExample(idToDelete).then(function() {
     refreshExamples();
   });
 };
-
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,115 @@
-var db = require("../models");
-
+const db = require("../models");
+const axios = require("axios");
 module.exports = function(app) {
+  
+  app.post("/api/users", function(req, res) {
+    var data = req.body
+    console.log(data)
+    db.User.findOrCreate({ where: { name: data.name, email: data.email }}
+    
+      ).then( function(result) {
+      res.json(result)
+    })
+  })
+  //*This is the route to add an Ingredient to a User's list
+  //!ROUTE
+  app.post("/api/add-ingredient/", function(req, res) {
+    let data = req.body
+    db.Ingredient.create
+    ({ label: data.label,
+      UserId: data.UserID
+    }).then( function(result) {
+      
+      res.json(result) 
+    
+    })
+  })
+  //*This is the route to remove an Ingredient from a User's List
+  //!ROUTE
+  app.delete("/api/remove-ingredient/:ingredient", function(req, res) {
+    let ingredient = req.params.ingredient
+    db.Ingredient.destroy
+    ({
+      where: { label: ingredient }
+    }).then( function(result) {
+      res.json(result)
+    })
+  })
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+  // app.get("/api/examples", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.json(dbExamples);
+  //   });
+  // });
+  // // Create a new example
+  // app.post("/api/examples", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
+  app.post("/api/get-recipes", function(req, res) {
+      //*This is the route to get the recipes from the api
+  //!ROUTE
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
+    //Build the api query. First build the q parameter
+    let data = JSON.parse(req.body.cook);
+    console.log(req.body.ingredient);
+    // let data = JSON.parse(req.body);
+    let data = req.body.ingredient;
+    let search = "q=";
+    let recipeArray = [];
+    let userIngredients = [];
+    console.log("\nincoming array: ",data);
+    for (let i = 0; i < data.length; i++) {
+      if (i < (data.length - 1)) {
+        search += data[i] + "+"
+        userIngredients.push(data[i])
+      } else { 
+        search += data[i]
+        userIngredients.push(data[i])
+      } 
+    }
+    recipeArray.push(userIngredients)
+    console.log("first stage recipeArray: ",recipeArray)
+    console.log("\nq:",search)
+    //At this point the q parameter is constructed. Now build the rest of the api query.
+    let appID = process.env.appID;
+    let appKey = process.env.appKey;
+    let  query = "https://api.edamam.com/search?" + search + "&app_id=" + appID + "&app_key=" + appKey + "&from=0&to=8"
+    console.log("query:",query)
+    //Make the API Call
+    axios.get(query)
+         .then( function(response) { 
+            let Recipe = response.data.hits
+                
+            for ( let i = 0; i < Recipe.length; i++ ) {
+              console.log("\n")
+              console.log(Recipe[i].recipe.label)
+              console.log(Recipe[i].recipe.image)
+              console.log(Recipe[i].recipe.url)
+              console.log(Recipe[i].recipe.ingredientLines)
+              console.log(Recipe[i].recipe.calories)
+              console.log(Recipe[i].recipe.yield)
+              console.log(Recipe[i].recipe.totalTime)
+              console.log("\n")
+              let recipe = { label: Recipe[i].recipe.label,
+                             image: Recipe[i].recipe.image,
+                             url: Recipe[i].recipe.url,
+                             ingredients: Recipe[i].recipe.ingredientLines,
+                             calories: Recipe[i].recipe.calories,
+                             servings: Recipe[i].recipe.yield,
+                             time: Recipe[i].recipe.totalTime
+                            }
+              recipeArray.push(recipe)              
+            }
+            res.send(recipeArray)
+          }).catch(function(error) { console.log(error)})
+  
+  })
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
